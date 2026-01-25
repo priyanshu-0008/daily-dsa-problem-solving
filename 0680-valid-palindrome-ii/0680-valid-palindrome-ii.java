5ab9b4ed93f91e88b3a55e3bbe51bf5831d398a2
priyanshu-0008 <pkverma012345678910@gmail.com>
2026-01-25 19:10:24 +0530

    Time: 4 ms (99.58%), Space: 47.6 MB (77.46%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return check(s,i+1,j) || check(s,i,j-1);
+            }
+        }
+        return true;
+    }
+    private boolean check(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+    return true;
+    }
+}
